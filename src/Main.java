f6d473a2c706d7f842cf9e48d494aa733d7d09c6
Дарья <dnovogodniaya@ya.ru>
2022-09-04 14:35:29 +0300

    с7-9 задания

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        //Задание4
+        //Задание7
         byte age=17;
-        if (age>=18){
-        System.out.println("Поздравляю ты совершеннолетний");
-    }else {
-            System.out.println("Возраст совершеннолетия ещё не наступил, надо подождать");
+        if (age>=2 && age<=6){
+        System.out.println("Ходишь в детский сад, тебе "+(age));
+    }else if (age>=7 && age<=18){
+            System.out.println("Ходишь в школу, тебе "+(age));
         }
-        //задание5
-        if (age>=18&&age<24){
-            System.out.println("Поздравляю ты закончил школу, можешь идти в вуз");
-        } else if ((age<18)&&(age>7)){
-            System.out.println("Возраст совершеннолетия ещё не наступил, ты учишься в школе");
-        } else {
-            System.out.println("Пора искать первую работу");
+        else if (age>18 && age<24){
+            System.out.println("Ходишь в ВУЗ, тебе "+(age));
         }
-        //задание6
-        short place=103;
-        if (place>60&&place<103){
-            System.out.println("Стоячее место в вагоне");
-        } else if (place<60){
-            System.out.println("Сидячее место в вагоне");
+        else {
+            System.out.println("Ходишь на работу, тебе "+(age));
+        }
+        //задание8
+        if (age>=5 && age<14){
+            System.out.println("Может кататься в сопровождение взрослых");
+        } else if (age<5){
+            System.out.println("Не может кататься");
         } else {
-            System.out.println("Вагон переполнен");
+            System.out.println("Может кататься без сопровождения");
+        }
+        //задание9
+        short one=11;
+        short two=2;
+        short free=34;
+        if (one>two && one>free){
+            System.out.println((one)+" Наибольшее");
+        } else if (two>one && two>free){
+            System.out.println((two)+" Наибольшее");
+        } else {
+            System.out.println((free)+" Наибольшее");
         }
     }
 }
